9ea61414f7a37d2e362c5e304a6b32ad9e1116dc
Srabani2002 <bharadwajsrabani2002@gmail.com>
2024-03-15 15:51:35 +0530

    homepage

--- ship-shop/src/main/java/com/shipshop/shipshop/HomePageController.java
+++ ship-shop/src/main/java/com/shipshop/shipshop/HomePageController.java
@@ -1,9 +1,11 @@
 package com.shipshop.shipshop;
 
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 public class HomePageController
 {
-    @GetMapping(value = "/")
+    @GetMapping(value = "home")
     public String getHomePage()
     {
         return "Hello World";
