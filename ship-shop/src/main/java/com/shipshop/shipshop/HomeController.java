9bd0236820a92b3c63dbe7c1c2376ab826510d1b
Srabani2002 <bharadwajsrabani2002@gmail.com>
2024-03-18 11:25:55 +0530

    homecontroller

--- ship-shop/src/main/java/com/shipshop/shipshop/HomeController.java
+++ ship-shop/src/main/java/com/shipshop/shipshop/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController
 { 
-    @RequestMapping("/home")
+    @RequestMapping("/srabani")
     public String home()
     {
         System.out.println("Inside home after");
