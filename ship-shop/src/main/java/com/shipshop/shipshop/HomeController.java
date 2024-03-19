357cd9d57070a3badf722b0044af9169a39b5014
hara-priya <harapriyarath0342@gmail.com>
2024-03-19 12:47:16 +0530

    HP - home url

--- ship-shop/src/main/java/com/shipshop/shipshop/HomeController.java
+++ ship-shop/src/main/java/com/shipshop/shipshop/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController
 { 
-    @RequestMapping("/srabani")
+    @RequestMapping("/")
     public String home()
     {
         System.out.println("Inside home after");
