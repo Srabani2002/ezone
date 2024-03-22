5be7de473a1006ea5b440b0463958c2af5d763d6
Srabani2002 <bharadwajsrabani2002@gmail.com>
2024-03-22 16:04:07 +0530

    SB : template

---
+++ src/test/java/com/ezone/ezone/EzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezone.ezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
